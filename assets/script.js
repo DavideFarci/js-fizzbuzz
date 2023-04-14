aa0db2b204e39493b195e90727aa4a3142556f42
DavideFarci <yank92.df@gmail.com>
2023-04-14 17:55:02 +0200

    add style in conditions

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,11 @@ let eleRow = document.querySelector(".row");
 for (let i = 1; i <= 100; i++) {
     console.log(i)
     if (i % 3 === 0 && i % 5 === 0) {
-        eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2">FizzBuzz</div>`
+        eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2" style="background-color: orange;">FizzBuzz</div>`;
     } else if (i % 3 === 0) {
-        eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2">Fizz</div>`
+        eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2" style="background-color: blue;">Fizz</div>`
     } else if (i % 5 === 0) {
-        eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2">Buzz</div>`
+        eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2" style="background-color: green;">Buzz</div>`
     } else {
         eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2">${i}</div>`
     }
