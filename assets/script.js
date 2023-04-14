19cd99a3b1c3b6f65bc9e9140a7df2e61f20c791
DavideFarci <yank92.df@gmail.com>
2023-04-14 16:57:28 +0200

    add first condition

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 const eleRow = document.querySelector(".row");
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     console.log(i)
     eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2">${i}</div>`
+
+    if (i % 3 === 0) {
+        eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2">"Fizz"</div>`;
+    }
 }
