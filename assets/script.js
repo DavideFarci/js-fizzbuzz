06e42d46eda306b321a340a830d17d18e05415e6
DavideFarci <yank92.df@gmail.com>
2023-04-14 17:42:34 +0200

    add all conditions

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,14 @@
-const eleRow = document.querySelector(".row");
+let eleRow = document.querySelector(".row");
 
 for (let i = 1; i <= 100; i++) {
     console.log(i)
-    eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2">${i}</div>`
-
-    if (i % 3 === 0) {
-        eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2">"Fizz"</div>`;
+    if (i % 3 === 0 && i % 5 === 0) {
+        eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2">FizzBuzz</div>`
+    } else if (i % 3 === 0) {
+        eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2">Fizz</div>`
+    } else if (i % 5 === 0) {
+        eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2">Buzz</div>`
+    } else {
+        eleRow.innerHTML = eleRow.innerHTML + `<div class="col-1 d-flex align-items-center justify-content-center g-2">${i}</div>`
     }
 }
